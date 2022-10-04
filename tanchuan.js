1d6d07dc90d76e8cd6d76db6c6e476560cb33e00
hhse <36848692+hhse@users.noreply.github.com>
2022-10-04 10:36:38 +0800

    Create tanchuan.js

---
+++ tanchuan.js
@@ -0,0 +1,9 @@
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert@2.1.2/dist/sweetalert.min.js"></script>
+    <script>
+      swal({
+        title: "免责申明",
+        text: "\n本站所展示内容均收集于网络\n\n仅供本人学习研究及收藏存档\n\n如有侵犯权益，敬请联系删",
+        icon: "success",
+        button: "同意上述申明",
+      });
+    </script>
